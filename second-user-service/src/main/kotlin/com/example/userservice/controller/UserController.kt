2aed2c5ade258b3a8b68433de913aa0f89f3189b
haril song <songkg7@gmail.com>
2023-08-16 21:04:15 +0900

    Add FilterConfig and modify respective services
    Introduced FilterConfig in the gateway service to add custom request and response headers. The FilterConfig class leverages Spring Cloud Gateway's fluent API to add these headers for communication with microservices.
    Also, updated the UserController in both first and second services for message mapping and respective header information logging.
    Spring application configuration in application.yml is commented out as routing is now defined programmatically in FilterConfig.

--- second-user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
+++ second-user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
@@ -1,6 +1,8 @@
 package com.example.userservice.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/second-service")
 class UserController {
 
+    private val log = LoggerFactory.getLogger(UserController::class.java)
+
     @GetMapping("/welcome")
     fun welcome(): String {
         return "Welcome to the Second service."
+    }
+
+    @GetMapping("/message")
+    fun message(@RequestHeader("second-request") header: String): String {
+        log.info(header)
+        return "Hello World in Second Service."
     }
 
 }
--- user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
@@ -1,6 +1,8 @@
 package com.example.userservice.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/first-service")
 class UserController {
 
+    private val log = LoggerFactory.getLogger(UserController::class.java)
+
     @GetMapping("/welcome")
     fun welcome(): String {
         return "Welcome to the First service."
+    }
+
+    @GetMapping("/message")
+    fun message(@RequestHeader("first-request") header: String): String {
+        log.info(header)
+        return "Hello World in First Service."
     }
 
 }
